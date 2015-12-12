4c95d726f01b7c7c0b945a66db2be2250395e9e5
Alex Ahern <alexahern@Pudboop.local>
2015-12-12 20:00:11 +0000

    Update with real data and set time

--- app/src/main/java/com/example/alexahern/raindrop/RainFragment.java
+++ app/src/main/java/com/example/alexahern/raindrop/RainFragment.java
@@ -1,14 +1,25 @@
 package com.example.alexahern.raindrop;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -17,6 +28,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -27,13 +40,38 @@ public class RainFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        TextView text = ((TextView) rootView.findViewById(R.id.last_updated_texview));
-        text.setText(getString(R.string.last_updated_message, getString(R.string.last_updated_value)));
-        updateWeather();
+        //TextView lastDateTextView = ((TextView) rootView.findViewById(R.id.last_updated_texview));
+        //lastDateTextView.setText(getString(R.string.last_updated_message, getString(R.string.last_updated_value)));
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
+        setCurrentTime();
+    }
+
+    public void setCurrentTime(){
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String currentTime = sdf.format(cal.getTime());
+        TextView lastDateTextView = ((TextView) getActivity().findViewById(R.id.last_updated_texview));
+        lastDateTextView.setText(getString(R.string.last_updated_message, currentTime));
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.rainfragment, menu);
     }
 
     private void updateWeather(){
@@ -41,11 +79,22 @@ public class RainFragment extends Fragment {
         rainTask.execute();
     }
 
-    public class getRainTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            updateWeather();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class getRainTask extends AsyncTask<String, Void, Double> {
         private final String LOG_TAG = getRainTask.class.getSimpleName();
 
         @Override
-        protected String doInBackground(String... params) {
+        protected Double doInBackground(String... params) {
         //if (params.length==0){
         //    return null;
         //}
@@ -77,7 +126,6 @@ public class RainFragment extends Fragment {
                     buffer.append(line);
                 }
                 rainForecastJsonStr = buffer.toString();
-                Log.d(LOG_TAG, rainForecastJsonStr);
 
             }
             catch(IOException e){
@@ -98,8 +146,31 @@ public class RainFragment extends Fragment {
                 }
             }
 
-        return null;
+            try{
+                return getPrecipPercent(rainForecastJsonStr);
+            }
+            catch(JSONException e){
+                Log.e(LOG_TAG,e.getMessage(),e);
+            }
+            return null;
         }
 
+        private Double getPrecipPercent(String jsonStr)
+                throws JSONException {
+            JSONObject forecastObject = new JSONObject(jsonStr);
+            JSONObject hourlyForecastObject = forecastObject.getJSONObject("daily")
+                    .getJSONArray("data")
+                    .getJSONObject(5);
+            Double currentPrecipProb = hourlyForecastObject.getDouble("precipProbability");
+            return currentPrecipProb*100;
+        }
+
+        protected void onPostExecute(Double result) {
+            if (result != null){
+                TextView text = (TextView) getActivity().findViewById(R.id.rain_textview);
+                text.setText(Integer.toString(result.intValue()));
+                text.append("%");
+            }
+        }
     }
 }
