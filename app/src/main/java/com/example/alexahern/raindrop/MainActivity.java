d018f5acbad858eb1eae9bc2b39863e4e9c2dc27
Alex Ahern <alexahern@Pudboop.local>
2015-12-13 15:40:50 +0000

    Revert "Adding SettingsActivity"
    This reverts commit 220a7e9a86c7929373f959578ffa5ed3e495481c.

--- app/src/main/java/com/example/alexahern/raindrop/MainActivity.java
+++ app/src/main/java/com/example/alexahern/raindrop/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.alexahern.raindrop;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -36,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            startActivity(new Intent(this,SettingsActivity.class));
             return true;
         }
 
